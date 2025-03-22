551f33af0c66e622e84a718d2df3aee413de4b26
Stas Petrov <petrovstas@mail.ru>
2025-03-22 16:42:36 +0500

    TODO: реализовать регистрацию ботов telegram, vk

---
+++ src/users/user.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
+import { User } from "./user.entity"
+import { UserDto } from "./user.dto"
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+  ) {}
+
+  async createUser(data: UserDto): Promise<void> {
+    console.log(data)
+    // const user = this.repository.create(data)
+    // const user = this.repository.create([data])
+    // return await this.repository.save(user)
+  }
+
+  // async getUser(id: number): Promise<User> {
+  //   const user = await this.repository.findOneBy({ id })
+  //   if (!user) throw new NotFoundException(Errors.displayId(id) + ErrorsMessages.USER_NOT_FOUND)
+  //   return user
+  // }
+  //
+  // async getUserByName(name: string): Promise<User> {
+  //   const user = await this.repository.findOneBy({ name: name.trim() })
+  //   if (!user) throw new NotFoundException(ErrorsMessages.USER_NOT_FOUND)
+  //   return user
+  // }
+  //
+  // async updateUser(id: number, data: UserDto): Promise<User> {
+  //   const result = await this.repository.update(id, data)
+  //   if (!result.affected) throw new NotFoundException(Errors.displayId(id) + ErrorsMessages.NOT_FOUND)
+  //   return await this.getUser(id)
+  // }
+  //
+  // async getAll(): Promise<User[]> {
+  //   return this.repository.find()
+  // }
+  //
+  // async deleteUser(id: number): Promise<void> {
+  //   const result = await this.repository.delete(id)
+  //   if (!result.affected) throw new NotFoundException(Errors.displayId(id) + ErrorsMessages.NOT_FOUND)
+  // }
+}
