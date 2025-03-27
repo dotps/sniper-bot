6f693dbdd2dea36716d0f5539440e970ca48abcc
Stas Petrov <petrovstas@mail.ru>
2025-03-27 20:02:59 +0500

    feat: add connect to bsc + create wallet

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from "@nestjs/common"
 import { AppService } from "./app.service"
+import { BlockchainService } from "./blockchain/blockchain.service"
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly blockchainService: BlockchainService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Get("test")
+  test(): string {
+    const address = this.blockchainService.createWallet()
+    console.log(address)
+    return address
   }
 }
--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -1,16 +1,43 @@
 import { Injectable } from "@nestjs/common"
-import { createPublicClient, http, PublicClient } from "viem"
-import { mainnet } from "viem/chains"
+import { createPublicClient, defineChain, Hex, http, PublicClient } from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
 
 @Injectable()
 export class BlockchainService {
-  private client: PublicClient
+  private readonly client: PublicClient
 
   constructor() {
     this.client = createPublicClient({
-      chain: mainnet,
+      chain: bsc,
       transport: http(),
     })
-    // TODO: реализовать подключение к binance
+  }
+
+  getClient(): PublicClient {
+    return this.client
+  }
+
+  createWallet(): Hex {
+    const privateKey = generatePrivateKey()
+    const account = privateKeyToAccount(privateKey)
+    return account.address
   }
 }
+
+const bsc = defineChain({
+  id: 56,
+  name: "Binance Smart Chain",
+  network: "bsc",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Binance Chain Native Token",
+    symbol: "BNB",
+  },
+  rpcUrls: {
+    default: { http: ["https://bsc-dataseed1.binance.org"] },
+    public: { http: ["https://bsc-dataseed1.binance.org"] },
+  },
+  blockExplorers: {
+    default: { name: "BscScan", url: "https://bscscan.com" },
+  },
+})
