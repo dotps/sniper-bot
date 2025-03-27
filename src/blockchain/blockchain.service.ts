a0d9fe6f654cd745a796c9f31c3add77c72b6880
Stas Petrov <petrovstas@mail.ru>
2025-03-27 20:42:25 +0500

    feat: add init many network client

--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -5,16 +5,30 @@ import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
 @Injectable()
 export class BlockchainService {
   private readonly client: PublicClient
+  private clients: Map<Blockchain, PublicClient> = new Map()
 
   constructor() {
-    this.client = createPublicClient({
+    this.initBlockchainClients()
+  }
+
+  private initBlockchainClients() {
+    const bscClient = createPublicClient({
       chain: bsc,
       transport: http(),
     })
+    const polygonClient = createPublicClient({
+      chain: polygon,
+      transport: http(),
+    })
+
+    this.clients.set(Blockchain.BSC, bscClient)
+    this.clients.set(Blockchain.POLYGON, polygonClient)
   }
 
-  getClient(): PublicClient {
-    return this.client
+  getClient(clientType: Blockchain): PublicClient {
+    const client = this.clients.get(clientType)
+    if (!client) throw Error("Клиент не найден.")
+    return client
   }
 
   createWallet(): Hex {
@@ -22,6 +36,11 @@ export class BlockchainService {
     const account = privateKeyToAccount(privateKey)
     return account.address
   }
+}
+
+enum Blockchain {
+  BSC = "bsc",
+  POLYGON = "polygon",
 }
 
 const bsc = defineChain({
@@ -39,5 +58,23 @@ const bsc = defineChain({
   },
   blockExplorers: {
     default: { name: "BscScan", url: "https://bscscan.com" },
+  },
+})
+
+const polygon = defineChain({
+  id: 137,
+  name: "Polygon",
+  network: "polygon",
+  nativeCurrency: {
+    decimals: 18,
+    name: "MATIC",
+    symbol: "MATIC",
+  },
+  rpcUrls: {
+    default: { http: ["https://polygon-rpc.com"] },
+    public: { http: ["https://polygon-rpc.com"] },
+  },
+  blockExplorers: {
+    default: { name: "PolygonScan", url: "https://polygonscan.com" },
   },
 })
