64b8657bfac4fb4099e5f74b562ae45235522e53
Stas Petrov <petrovstas@mail.ru>
2025-04-01 20:03:59 +0500

    TODO: продолжить делать ReplicateCommand

---
+++ src/commands/ReplicateCommand.ts
@@ -0,0 +1,44 @@
+import { ICommand } from "./ICommand"
+import { ResponseData } from "../data/ResponseData"
+import { Command } from "./CommandHandler"
+import { User } from "../users/user.entity"
+import { ResponseBotError } from "../errors/ResponseBotError"
+import { Logger } from "../utils/Logger"
+import { Commands } from "./Commands"
+import { WalletService } from "../blockchain/wallet.service"
+import { isAddress } from "viem"
+
+export class ReplicateCommand implements ICommand {
+  private readonly walletService: WalletService
+  private readonly commandData: Command
+  private readonly user: User
+  private readonly messages = {
+    NEED_WALLET: `Требуется адрес кошелька.\n${Commands.FOLLOW} адрес_кошелька`,
+    SUCCESS: "Подписка на кошелек успешно оформлена.",
+  } as const
+
+  constructor(walletService: WalletService, user: User, commandData: Command) {
+    this.user = user
+    this.walletService = walletService
+    this.commandData = commandData
+  }
+
+  // TODO: продолжить делать ReplicateCommand
+
+  async execute(): Promise<ResponseData | null> {
+    const [walletAddress] = this.commandData.params || []
+    if (!walletAddress || !isAddress(walletAddress)) return new ResponseData(this.messages.NEED_WALLET)
+
+    try {
+      await this.walletService.createFollowWallet(walletAddress, this.user.id)
+      return new ResponseData(this.messages.SUCCESS)
+    } catch (error) {
+      if (error instanceof ResponseBotError) {
+        return new ResponseData(error.message)
+      } else {
+        Logger.error(error)
+        return null
+      }
+    }
+  }
+}
