6dbe2ea7f88c67820a5a290916534c922a2bb357
Stas Petrov <petrovstas@mail.ru>
2025-04-02 11:47:54 +0500

    feat: add db error handler

---
+++ src/errors/DBError.ts
@@ -0,0 +1,33 @@
+import { ResponseBotError } from "./ResponseBotError"
+
+export class DBError {
+  static handle(error: unknown, textBefore?: string) {
+    const currentError = error as { code?: unknown }
+    const code: string = currentError?.code?.toString() || ""
+
+    if (!code || !errors[code]) throw error
+
+    const exceptionMethod = errors[code].exception || ResponseBotError
+    const messageText = textBefore + errors[code].message
+    const message = errors[code].message ? messageText : ""
+
+    throw new exceptionMethod(message)
+  }
+}
+
+const errors: ErrorsType = {
+  "23505": {
+    message: "такое значение уже присутствует.",
+    exception: ResponseBotError,
+  },
+  "23503": { message: "такое значение не найдено." },
+}
+
+type ErrorsType = {
+  [key: string]: {
+    message: string
+    exception?: ExceptionType
+  }
+}
+
+type ExceptionType = new (message?: string) => Error
